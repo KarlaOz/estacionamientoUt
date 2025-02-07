7381a50c8088e36ad7f6e1e648c139c15dc89baf
KarlaOz <142529957+KarlaOz@users.noreply.github.com>
2025-02-07 16:37:52 -0600

    Init
    Primera version

---
+++ resources/views/index.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>UTN</title>
+    <link rel="stylesheet" href="style.css"></head>
+<body>
+    <header>
+                    <img class="login" src="imagenes/login.png" alt="imagen login">
+
+    </header>
+ <div class="portada">
+        <h2>Universidad Tecnologica de Nayarit</h2>
+    </div>
+    <section id="tipos">
+        <h2 class="section-title">Bienvenido a la UTN</h2>
+        <div class="cards">
+            <div class="card">
+                <div class="card-content">
+                    <h3>Mision</h3>
+                    <p>mision</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <footer id="contacto">
+        <p>(c) 2024 Universidad Tecnológica de Nayarit. Derechos Reservados</p>
+
+    </footer>
+</body>
+</html>
