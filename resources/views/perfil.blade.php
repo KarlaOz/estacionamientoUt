a468288fdd48f3353e135eb3c9d1a059042d5836
JGEB-DEV <tic-310049@utnay.edu.mx>
2025-02-08 22:38:22 -0700

    construccion total

--- resources/views/perfil.blade.php
+++ resources/views/perfil.blade.php
@@ -1,22 +1,7 @@
-<!DOCTYPE html>
-<html lang="es">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>UTN</title>
-        <link rel="stylesheet" href="style.css">
-    </head>
-
-    <body>
-        <header>
-            <h1>Perfil del usuario</h1>
-
-        </header>
-
-
-        <footer id="contacto">
-            <p>(c) 2024 Universidad Tecnológica de Nayarit. Derechos Reservados</p>
-
-        </footer>
-    </body>
-</html>
+@extends('plantilla')
+@section('title', 'Perfil - UTN')
+@section('contenido')
+    <section>
+        <h class="section-title"1>Perfil de Usuario</h1>
+    </section>
+@endsection
