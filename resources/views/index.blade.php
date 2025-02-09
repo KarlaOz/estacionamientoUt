faa9f97c7b1fbe8fb45776e7f7cb779ffdcd1f29
EGM Studios <106367680+EJGMStudios23@users.noreply.github.com>
2025-02-08 19:27:58 -0700

    Reconfiguracion del proyecto

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,13 +1,16 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>UTN</title>
-    <link rel="stylesheet" href="style.css"></head>
+    <link rel="stylesheet" href="style.css">
+</head>
+
 <body>
     <header>
-                    <img class="login" src="imagenes/login.png" alt="imagen login">
+        <img class="login" src="imagenes/login.png" alt="imagen login">
 
     </header>
     <div class="portada">
@@ -20,15 +23,15 @@
                 <div class="card-content">
                     <h3>Misión</h3>
                     <p style="text-align: justify;">Formar profesionistas competitivos mediante programas de educación superior científicos y tecnológicos, sutentados en un modelo integral y humanista, con un enfoque basado en competencias profesionales y con un sentido de innovación para el desarrollo sustentable del entorno .</br>
-                    Vincular la institución con los sectores público, social y privado para fortalecer los programas educativos y contribuir con el desarrollo de servicios especializados, asistencia técnica y transferencia de tecnología, a través de la investigación aplicada para impulsar el desarrollo de la región.
-</p>
+                        Vincular la institución con los sectores público, social y privado para fortalecer los programas educativos y contribuir con el desarrollo de servicios especializados, asistencia técnica y transferencia de tecnología, a través de la investigación aplicada para impulsar el desarrollo de la región.
+                    </p>
                 </div>
             </div>
             <div class="card">
                 <div class="card-content">
                     <h3>Visión</h3>
                     <p style="text-align: justify;">Ser una institución con excelencia académica reconocida por entidades nacionales e internacionales, comprometida con el desarrollo humano equitativo e incluyente que forme profesionistas competentes con sentido de responsabilidad social.
-</p>
+                    </p>
                 </div>
             </div>
             <div class="card">
@@ -36,12 +39,12 @@
                     <h3>Valores</h3>
                     <p style="text-align: justify;">Compromiso: Sentido de pertenencia a la organización para generar una identidad universitaria.</br>
 
-                    Solidaridad: Colaboración mutua que existe entre la comunidad universitaria para el logro de objetivos comunes.</br>
-                    Respeto: Reconocimiento al valor, dignidad y derecho entre alumnos, profesores, administrativos y su entorno físico y social.</br>
-                    Responsabilidad: Cumplimiento de derechos y obligaciones de cada integrante de la comunidad universitaria.</br>
-                    Honestidad: Ser auténtico en sus acciones, congruentes de lo que dicen con lo que hacen y sienten, dentro y fuera de la institución.</br>
-                    Lealtad: Congruencia con las políticas institucionales para el buen actuar de los miembros de la comunidad universitaria.
-</p>
+                        Solidaridad: Colaboración mutua que existe entre la comunidad universitaria para el logro de objetivos comunes.</br>
+                        Respeto: Reconocimiento al valor, dignidad y derecho entre alumnos, profesores, administrativos y su entorno físico y social.</br>
+                        Responsabilidad: Cumplimiento de derechos y obligaciones de cada integrante de la comunidad universitaria.</br>
+                        Honestidad: Ser auténtico en sus acciones, congruentes de lo que dicen con lo que hacen y sienten, dentro y fuera de la institución.</br>
+                        Lealtad: Congruencia con las políticas institucionales para el buen actuar de los miembros de la comunidad universitaria.
+                    </p>
                 </div>
             </div>
         </div>
@@ -53,4 +56,5 @@
 
     </footer>
 </body>
+
 </html>
--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
-
-Route::get('/cajones',function(){
-    return view('cajones');
-});
-
