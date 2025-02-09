c2beb4e86a9cb6a8635bd1656ce133e7713db893
JGEB-DEV <tic-310049@utnay.edu.mx>
2025-02-09 15:10:26 -0700

    nuevo cajones

--- resources/views/cajones.blade.php
+++ resources/views/cajones.blade.php
@@ -1,401 +1,109 @@
 @extends('plantilla')
 @section('title', 'Estacionamiento - UTN')
+
+<head>
+    <link rel="stylesheet" href="estilos/cajones.css">
+</head>
+
 @section('contenido')
     <section>
-        <h1 class="section-title">Cajones</h1>
+        <h1 class="section-title">Estación X</h1>
         <div class="cards">
             <div class="card-container">
-                <h2>Estación 1</h2>
+                <h2>Espacios</h2>
                 <table class="table-decorator">
                     <tr>
                         <td>
-                            <img src="imagenes/unknown.png" alt="">
+                            <img src="imagenes/puestoocupado.png" alt="">
                         </td>
                         <td>
-                            <img src="imagenes/unknown.png" alt="">
+                            <img src="imagenes/puestolibre.png" alt="">
                         </td>
                         <td>
-                            <img src="imagenes/unknown.png" alt="">
+                            <img src="imagenes/puestoocupado.png" alt="">
                         </td>
                         <td>
-                            <img src="imagenes/unknown.png" alt="">
+                            <img src="imagenes/puestolibre.png" alt="">
                         </td>
                         <td>
-                            <img src="imagenes/unknown.png" alt="">
+                            <img src="imagenes/puestolibre.png" alt="">
                         </td>
                         <td>
-                            <img src="imagenes/unknown.png" alt="">
+                            <img src="imagenes/puestolibre.png" alt="">
                         </td>
                         <td>
-                            <img src="imagenes/unknown.png" alt="">
+                            <img src="imagenes/puestoocupado.png" alt="">
                         </td>
                         <td>
-                            <img src="imagenes/unknown.png" alt="">
+                            <img src="imagenes/puestoocupado.png" alt="">
                         </td>
                         <td>
-                            <img src="imagenes/unknown.png" alt="">
+                            <img src="imagenes/puestoocupado.png" alt="">
                         </td>
                         <td>
-                            <img src="imagenes/unknown.png" alt="">
+                            <img src="imagenes/puestolibre.png" alt="">
                         </td>
                         <td>
-                            <img src="imagenes/unknown.png" alt="">
+                            <img src="imagenes/puestolibre.png" alt="">
                         </td>
                         <td>
-                            <img src="imagenes/unknown.png" alt="">
+                            <img src="imagenes/puestolibre.png" alt="">
                         </td>
                         <td>
-                            <img src="imagenes/unknown.png" alt="">
+                            <img src="imagenes/puestolibre.png" alt="">
                         </td>
                         <td>
-                            <img src="imagenes/unknown.png" alt="">
+                            <img src="imagenes/puestolibre.png" alt="">
                         </td>
                         <td>
-                            <img src="imagenes/unknown.png" alt="">
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <img src="imagenes/unknown.png" alt="">
-                        </td>
-                        <td>
-                            <img src="imagenes/unknown.png" alt="">
-                        </td>
-                        <td>
-                            <img src="imagenes/unknown.png" alt="">
-                        </td>
-                        <td>
-                            <img src="imagenes/unknown.png" alt="">
-                        </td>
-                        <td>
-                            <img src="imagenes/unknown.png" alt="">
-                        </td>
-                        <td>
-                            <img src="imagenes/unknown.png" alt="">
-                        </td>
-                        <td>
-                            <img src="imagenes/unknown.png" alt="">
-                        </td>
-                        <td>
-                            <img src="imagenes/unknown.png" alt="">
-                        </td>
-                        <td>
-                            <img src="imagenes/unknown.png" alt="">
-                        </td>
-                        <td>
-                            <img src="imagenes/unknown.png" alt="">
-                        </td>
-                        <td>
-                            <img src="imagenes/unknown.png" alt="">
-                        </td>
-                        <td>
-                            <img src="imagenes/unknown.png" alt="">
-                        </td>
-                        <td>
-                            <img src="imagenes/unknown.png" alt="">
-                        </td>
-                        <td>
-                            <img src="imagenes/unknown.png" alt="">
-                        </td>
-                        <td>
-                            <img src="imagenes/unknown.png" alt="">
-                        </td>
-                    </tr>
-                </table>
-            </div>
-            <div class="card-container">
-                <h2>Estación 2</h2>
-                <table class="table-decorator">
-                    <tr>
-                        <td>
-                            <img src="imagenes/unknown.png" alt="">
-                        </td>
-                        <td>
-                            <img src="imagenes/unknown.png" alt="">
-                        </td>
-                        <td>
-                            <img src="imagenes/unknown.png" alt="">
-                        </td>
-                        <td>
-                            <img src="imagenes/unknown.png" alt="">
-                        </td>
-                        <td>
-                            <img src="imagenes/unknown.png" alt="">
-                        </td>
-                        <td>
-                            <img src="imagenes/unknown.png" alt="">
-                        </td>
-                        <td>
-                            <img src="imagenes/unknown.png" alt="">
-                        </td>
-                        <td>
-                            <img src="imagenes/unknown.png" alt="">
-                        </td>
-                        <td>
-                            <img src="imagenes/unknown.png" alt="">
-                        </td>
-                        <td>
-                            <img src="imagenes/unknown.png" alt="">
-                        </td>
-                        <td>
-                            <img src="imagenes/unknown.png" alt="">
-                        </td>
-                        <td>
-                            <img src="imagenes/unknown.png" alt="">
-                        </td>
-                        <td>
-                            <img src="imagenes/unknown.png" alt="">
-                        </td>
-                        <td>
-                            <img src="imagenes/unknown.png" alt="">
-                        </td>
-                        <td>
-                            <img src="imagenes/unknown.png" alt="">
+                            <img src="imagenes/puestolibre.png" alt="">
                         </td>
                     </tr>
                     <tr>
                         <td>
-                            <img src="imagenes/unknown.png" alt="">
+                            <img src="imagenes/puestolibre.png" alt="">
                         </td>
                         <td>
-                            <img src="imagenes/unknown.png" alt="">
+                            <img src="imagenes/puestoocupado.png" alt="">
                         </td>
                         <td>
-                            <img src="imagenes/unknown.png" alt="">
+                            <img src="imagenes/puestolibre.png" alt="">
                         </td>
                         <td>
-                            <img src="imagenes/unknown.png" alt="">
+                            <img src="imagenes/puestolibre.png" alt="">
                         </td>
                         <td>
-                            <img src="imagenes/unknown.png" alt="">
+                            <img src="imagenes/puestolibre.png" alt="">
                         </td>
                         <td>
-                            <img src="imagenes/unknown.png" alt="">
+                            <img src="imagenes/puestolibre.png" alt="">
                         </td>
                         <td>
-                            <img src="imagenes/unknown.png" alt="">
+                            <img src="imagenes/puestoocupado.png" alt="">
                         </td>
                         <td>
-                            <img src="imagenes/unknown.png" alt="">
+                            <img src="imagenes/puestolibre.png" alt="">
                         </td>
                         <td>
-                            <img src="imagenes/unknown.png" alt="">
+                            <img src="imagenes/puestolibre.png" alt="">
                         </td>
                         <td>
-                            <img src="imagenes/unknown.png" alt="">
+                            <img src="imagenes/puestolibre.png" alt="">
                         </td>
                         <td>
-                            <img src="imagenes/unknown.png" alt="">
+                            <img src="imagenes/puestoocupado.png" alt="">
                         </td>
                         <td>
-                            <img src="imagenes/unknown.png" alt="">
+                            <img src="imagenes/puestoocupado.png" alt="">
                         </td>
                         <td>
-                            <img src="imagenes/unknown.png" alt="">
+                            <img src="imagenes/puestoocupado.png" alt="">
                         </td>
                         <td>
-                            <img src="imagenes/unknown.png" alt="">
+                            <img src="imagenes/puestolibre.png" alt="">
                         </td>
                         <td>
-                            <img src="imagenes/unknown.png" alt="">
-                        </td>
-                    </tr>
-                </table>
-            </div>
-            <div class="card-container">
-                <h2>Estación 3</h2>
-                <table class="table-decorator">
-                    <tr>
-                        <td>
-                            <img src="imagenes/unknown.png" alt="">
-                        </td>
-                        <td>
-                            <img src="imagenes/unknown.png" alt="">
-                        </td>
-                        <td>
-                            <img src="imagenes/unknown.png" alt="">
-                        </td>
-                        <td>
-                            <img src="imagenes/unknown.png" alt="">
-                        </td>
-                        <td>
-                            <img src="imagenes/unknown.png" alt="">
-                        </td>
-                        <td>
-                            <img src="imagenes/unknown.png" alt="">
-                        </td>
-                        <td>
-                            <img src="imagenes/unknown.png" alt="">
-                        </td>
-                        <td>
-                            <img src="imagenes/unknown.png" alt="">
-                        </td>
-                        <td>
-                            <img src="imagenes/unknown.png" alt="">
-                        </td>
-                        <td>
-                            <img src="imagenes/unknown.png" alt="">
-                        </td>
-                        <td>
-                            <img src="imagenes/unknown.png" alt="">
-                        </td>
-                        <td>
-                            <img src="imagenes/unknown.png" alt="">
-                        </td>
-                        <td>
-                            <img src="imagenes/unknown.png" alt="">
-                        </td>
-                        <td>
-                            <img src="imagenes/unknown.png" alt="">
-                        </td>
-                        <td>
-                            <img src="imagenes/unknown.png" alt="">
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <img src="imagenes/unknown.png" alt="">
-                        </td>
-                        <td>
-                            <img src="imagenes/unknown.png" alt="">
-                        </td>
-                        <td>
-                            <img src="imagenes/unknown.png" alt="">
-                        </td>
-                        <td>
-                            <img src="imagenes/unknown.png" alt="">
-                        </td>
-                        <td>
-                            <img src="imagenes/unknown.png" alt="">
-                        </td>
-                        <td>
-                            <img src="imagenes/unknown.png" alt="">
-                        </td>
-                        <td>
-                            <img src="imagenes/unknown.png" alt="">
-                        </td>
-                        <td>
-                            <img src="imagenes/unknown.png" alt="">
-                        </td>
-                        <td>
-                            <img src="imagenes/unknown.png" alt="">
-                        </td>
-                        <td>
-                            <img src="imagenes/unknown.png" alt="">
-                        </td>
-                        <td>
-                            <img src="imagenes/unknown.png" alt="">
-                        </td>
-                        <td>
-                            <img src="imagenes/unknown.png" alt="">
-                        </td>
-                        <td>
-                            <img src="imagenes/unknown.png" alt="">
-                        </td>
-                        <td>
-                            <img src="imagenes/unknown.png" alt="">
-                        </td>
-                        <td>
-                            <img src="imagenes/unknown.png" alt="">
-                        </td>
-                    </tr>
-                </table>
-            </div>
-            <div class="card-container">
-                <h2>Estación 4</h2>
-                <table class="table-decorator">
-                    <tr>
-                        <td>
-                            <img src="imagenes/unknown.png" alt="">
-                        </td>
-                        <td>
-                            <img src="imagenes/unknown.png" alt="">
-                        </td>
-                        <td>
-                            <img src="imagenes/unknown.png" alt="">
-                        </td>
-                        <td>
-                            <img src="imagenes/unknown.png" alt="">
-                        </td>
-                        <td>
-                            <img src="imagenes/unknown.png" alt="">
-                        </td>
-                        <td>
-                            <img src="imagenes/unknown.png" alt="">
-                        </td>
-                        <td>
-                            <img src="imagenes/unknown.png" alt="">
-                        </td>
-                        <td>
-                            <img src="imagenes/unknown.png" alt="">
-                        </td>
-                        <td>
-                            <img src="imagenes/unknown.png" alt="">
-                        </td>
-                        <td>
-                            <img src="imagenes/unknown.png" alt="">
-                        </td>
-                        <td>
-                            <img src="imagenes/unknown.png" alt="">
-                        </td>
-                        <td>
-                            <img src="imagenes/unknown.png" alt="">
-                        </td>
-                        <td>
-                            <img src="imagenes/unknown.png" alt="">
-                        </td>
-                        <td>
-                            <img src="imagenes/unknown.png" alt="">
-                        </td>
-                        <td>
-                            <img src="imagenes/unknown.png" alt="">
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <img src="imagenes/unknown.png" alt="">
-                        </td>
-                        <td>
-                            <img src="imagenes/unknown.png" alt="">
-                        </td>
-                        <td>
-                            <img src="imagenes/unknown.png" alt="">
-                        </td>
-                        <td>
-                            <img src="imagenes/unknown.png" alt="">
-                        </td>
-                        <td>
-                            <img src="imagenes/unknown.png" alt="">
-                        </td>
-                        <td>
-                            <img src="imagenes/unknown.png" alt="">
-                        </td>
-                        <td>
-                            <img src="imagenes/unknown.png" alt="">
-                        </td>
-                        <td>
-                            <img src="imagenes/unknown.png" alt="">
-                        </td>
-                        <td>
-                            <img src="imagenes/unknown.png" alt="">
-                        </td>
-                        <td>
-                            <img src="imagenes/unknown.png" alt="">
-                        </td>
-                        <td>
-                            <img src="imagenes/unknown.png" alt="">
-                        </td>
-                        <td>
-                            <img src="imagenes/unknown.png" alt="">
-                        </td>
-                        <td>
-                            <img src="imagenes/unknown.png" alt="">
-                        </td>
-                        <td>
-                            <img src="imagenes/unknown.png" alt="">
-                        </td>
-                        <td>
-                            <img src="imagenes/unknown.png" alt="">
+                            <img src="imagenes/puestolibre.png" alt="">
                         </td>
                     </tr>
                 </table>
