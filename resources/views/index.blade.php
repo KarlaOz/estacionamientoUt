fba2c6004f478689b5d20f7ace8aec8f8c22f420
KarlaOz <142529957+KarlaOz@users.noreply.github.com>
2025-02-08 22:30:20 -0600

    actualice login

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,7 +10,9 @@
 
 <body>
     <header>
-        <img class="login" src="imagenes/login.png" alt="imagen login">
+        <a href="/login">
+            <img class="login" src="imagenes/login.png" alt="imagen login">
+        </a>
 
     </header>
     <div class="portada">
@@ -22,28 +24,42 @@
             <div class="card">
                 <div class="card-content">
                     <h3>Misión</h3>
-                    <p style="text-align: justify;">Formar profesionistas competitivos mediante programas de educación superior científicos y tecnológicos, sutentados en un modelo integral y humanista, con un enfoque basado en competencias profesionales y con un sentido de innovación para el desarrollo sustentable del entorno .</br>
-                        Vincular la institución con los sectores público, social y privado para fortalecer los programas educativos y contribuir con el desarrollo de servicios especializados, asistencia técnica y transferencia de tecnología, a través de la investigación aplicada para impulsar el desarrollo de la región.
+                    <p style="text-align: justify;">Formar profesionistas competitivos mediante programas de educación
+                        superior científicos y tecnológicos, sutentados en un modelo integral y humanista, con un
+                        enfoque basado en competencias profesionales y con un sentido de innovación para el desarrollo
+                        sustentable del entorno .</br>
+                        Vincular la institución con los sectores público, social y privado para fortalecer los programas
+                        educativos y contribuir con el desarrollo de servicios especializados, asistencia técnica y
+                        transferencia de tecnología, a través de la investigación aplicada para impulsar el desarrollo
+                        de la región.
                     </p>
                 </div>
             </div>
             <div class="card">
                 <div class="card-content">
                     <h3>Visión</h3>
-                    <p style="text-align: justify;">Ser una institución con excelencia académica reconocida por entidades nacionales e internacionales, comprometida con el desarrollo humano equitativo e incluyente que forme profesionistas competentes con sentido de responsabilidad social.
+                    <p style="text-align: justify;">Ser una institución con excelencia académica reconocida por
+                        entidades nacionales e internacionales, comprometida con el desarrollo humano equitativo e
+                        incluyente que forme profesionistas competentes con sentido de responsabilidad social.
                     </p>
                 </div>
             </div>
             <div class="card">
                 <div class="card-content">
                     <h3>Valores</h3>
-                    <p style="text-align: justify;">Compromiso: Sentido de pertenencia a la organización para generar una identidad universitaria.</br>
+                    <p style="text-align: justify;">Compromiso: Sentido de pertenencia a la organización para generar
+                        una identidad universitaria.</br>
 
-                        Solidaridad: Colaboración mutua que existe entre la comunidad universitaria para el logro de objetivos comunes.</br>
-                        Respeto: Reconocimiento al valor, dignidad y derecho entre alumnos, profesores, administrativos y su entorno físico y social.</br>
-                        Responsabilidad: Cumplimiento de derechos y obligaciones de cada integrante de la comunidad universitaria.</br>
-                        Honestidad: Ser auténtico en sus acciones, congruentes de lo que dicen con lo que hacen y sienten, dentro y fuera de la institución.</br>
-                        Lealtad: Congruencia con las políticas institucionales para el buen actuar de los miembros de la comunidad universitaria.
+                        Solidaridad: Colaboración mutua que existe entre la comunidad universitaria para el logro de
+                        objetivos comunes.</br>
+                        Respeto: Reconocimiento al valor, dignidad y derecho entre alumnos, profesores, administrativos
+                        y su entorno físico y social.</br>
+                        Responsabilidad: Cumplimiento de derechos y obligaciones de cada integrante de la comunidad
+                        universitaria.</br>
+                        Honestidad: Ser auténtico en sus acciones, congruentes de lo que dicen con lo que hacen y
+                        sienten, dentro y fuera de la institución.</br>
+                        Lealtad: Congruencia con las políticas institucionales para el buen actuar de los miembros de la
+                        comunidad universitaria.
                     </p>
                 </div>
             </div>
